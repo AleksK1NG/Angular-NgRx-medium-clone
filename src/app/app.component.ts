3fea071bd33fba55b8ea2087e29d0f4336e8e6c8
Alexander Bryksin <abryksin@luxoft.com>
2020-11-20 16:43:54 +0300

    feat: add tslint setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'medium-clone';
+  title = 'medium-clone'
+
+  handleClick(e: Event) {
+    console.log('click ', e.target)
+    this.title = ' Cool =D'
+  }
 }
