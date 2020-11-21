d4998556120cb906ad814b926425c189adcffbdd
Alexander Bryksin <abryksin@luxoft.com>
2020-11-21 12:53:50 +0300

    feat: add auth actions and selectors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { NgModule } from '@angular/core'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module'
 import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { environment } from '../environments/environment'
+import * as auth from './auth/store/authReducer'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, StoreModule.forRoot({}, {})],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule,
+    StoreModule.forRoot({ auth: auth.reducer }, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
