e7a41117481e0d01496061661b2e0bb30c7a2d95
Alexander Bryksin <abryksin@luxoft.com>
2020-11-21 19:29:34 +0300

    feat: add persistanceService

---
+++ src/app/shared/modules/error-messages/componnets/error-message/error-message.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core'
+import { BackendErrorsInterface } from '../../../../types/interfaces'
+
+@Component({
+  selector: 'app-error-message',
+  templateUrl: './error-message.component.html',
+  styleUrls: ['./error-message.component.scss'],
+})
+export class ErrorMessageComponent implements OnInit {
+  constructor() {}
+  @Input() errors: BackendErrorsInterface | null
+  errorMessages: string[]
+
+  ngOnInit(): void {
+    this.errorMessages = Object.keys(this.errors).map((name, i) => {
+      const messages = this.errors[name].join(', ')
+      return `${name} ${messages}`
+    })
+  }
+}
