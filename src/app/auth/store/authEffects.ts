efff360db82fedace8dc7666652615793c3aaa9d
Alexander Bryksin <abryksin@luxoft.com>
2020-11-21 19:29:34 +0300

    feat: add persistanceService

--- src/app/auth/store/authEffects.ts
+++ src/app/auth/store/authEffects.ts
@@ -5,10 +5,11 @@ import { catchError, map, switchMap } from 'rxjs/operators'
 import { AuthService } from '../services/auth.service'
 import { of } from 'rxjs'
 import { HttpErrorResponse } from '@angular/common/http'
+import { PersistenceService } from '../../shared/services/persistence.service'
 
 @Injectable()
 export class AuthEffects {
-  constructor(private actions$: Actions, private authService: AuthService) {}
+  constructor(private actions$: Actions, private authService: AuthService, private persistanceService: PersistenceService) {}
 
   registerUserEffect$ = createEffect(() =>
     this.actions$.pipe(
@@ -16,6 +17,7 @@ export class AuthEffects {
       switchMap(({ request }) => {
         return this.authService.registerUser(request).pipe(
           map((response) => {
+            this.persistanceService.set('access-token', response.token)
             return registerSuccessAction({ currentUser: response })
           }),
           catchError((err: HttpErrorResponse) => {
