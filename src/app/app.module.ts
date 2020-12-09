aa9c056ad66372c3461035bc3ed06862fdb41391
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-09 19:17:51 +0300

    feat: add Auth Interceptor Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { AuthModule } from './auth/auth.module'
@@ -8,9 +7,11 @@ import { StoreModule } from '@ngrx/store'
 import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { environment } from '../environments/environment'
 import * as auth from './auth/store/authReducer'
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { EffectsModule } from '@ngrx/effects'
 import { TopBarModule } from './shared/modules/top-bar/top-bar.module'
+import { PersistenceService } from './shared/services/persistence.service'
+import { AuthInterceptorService } from './shared/services/auth-interceptor.service'
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,7 +25,7 @@ import { TopBarModule } from './shared/modules/top-bar/top-bar.module'
     EffectsModule.forRoot([]),
     TopBarModule,
   ],
-  providers: [],
+  providers: [PersistenceService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
