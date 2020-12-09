3219bb4b0e026ab03a0d31dde6d7dd3baa3cae50
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-09 17:23:04 +0300

    feat: add login form component

---
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { select, Store } from '@ngrx/store'
+import { Observable } from 'rxjs'
+import { BackendErrorsInterface } from '../../../shared/types/interfaces'
+import { authErrorsSelector, isSubmittingSelector } from '../../store/authSelectors'
+import { LoginRequest } from '../../store/authInterfaces'
+import { loginRequestAction } from '../../store/authActions'
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss'],
+})
+export class LoginFormComponent implements OnInit, OnChanges {
+  constructor(private fb: FormBuilder, private store: Store) {}
+  form: FormGroup
+  isSubmitting$: Observable<boolean>
+  backendErrors$: Observable<BackendErrorsInterface> | null
+
+  initializeForm() {
+    this.form = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    })
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    this.backendErrors$ = this.store.pipe(select(authErrorsSelector))
+  }
+
+  onSubmit(e: Event) {
+    e.preventDefault()
+    const request: LoginRequest = { user: this.form.value }
+    this.store.dispatch(loginRequestAction({ request }))
+  }
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('on change ', changes)
+  }
+}
