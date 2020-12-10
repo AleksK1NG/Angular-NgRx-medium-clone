3afc21624906591578ef9f3367b99922c8afda96
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-10 13:28:42 +0300

    feat: add error message module

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -7,9 +7,16 @@ import { StoreModule } from '@ngrx/store'
 import { reducers } from './store/feedReducer'
 import { FeedService } from './services/feed.service'
 import { RouterModule } from '@angular/router'
+import { ErrorMessagesModule } from '../backend-error-messages/error-messages.module'
 
 @NgModule({
-  imports: [CommonModule, EffectsModule.forFeature([FeedEffects]), StoreModule.forFeature('feed', reducers), RouterModule],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([FeedEffects]),
+    StoreModule.forFeature('feed', reducers),
+    RouterModule,
+    ErrorMessagesModule,
+  ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
   providers: [FeedService],
