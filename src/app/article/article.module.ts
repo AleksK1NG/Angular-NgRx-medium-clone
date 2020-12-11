0e726912d901e90d938041f2830c3f010427bbe7
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-11 12:25:10 +0300

    feat: add article selectors

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { ArticleComponent } from './components/article/article.component'
+import { EffectsModule } from '@ngrx/effects'
+import { ArticleEffects } from './store/articleEffects'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from './store/articleReducer'
+import { ArticleService as SharedArticleService } from '../shared/services/article.service'
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EffectsModule.forFeature([ArticleEffects]), StoreModule.forFeature('articles', reducers)],
   declarations: [ArticleComponent],
   exports: [ArticleComponent],
+  providers: [SharedArticleService],
 })
 export class ArticleModule {}
--- src/app/shared/types/appState.ts
+++ src/app/shared/types/appState.ts
@@ -1,9 +1,11 @@
 import { AuthState } from '../../auth/store/authInterfaces'
 import { FeedState } from '../modules/feed/store/feedReducer'
 import { PopularTagsState } from '../modules/popular-tags/store/popularTagsReducer'
+import { ArticleState } from '../../article/store/articleReducer'
 
 export interface AppState {
   auth: AuthState
   feed: FeedState
   popularTags: PopularTagsState
+  article: ArticleState
 }
