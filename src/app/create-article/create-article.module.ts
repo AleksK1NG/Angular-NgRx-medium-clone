e068fe0566400228d66c5a5314a643be0d99bd7c
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-11 16:17:48 +0300

    feat: add create article reducer

--- src/app/create-article/create-article.module.ts
+++ src/app/create-article/create-article.module.ts
@@ -4,6 +4,10 @@ import { CreateArticleComponent } from './components/create-article/create-artic
 import { RouterModule, Routes } from '@angular/router'
 import { ArticleFormModule } from '../shared/modules/article-form/article-form.module'
 import { CreateArticleService } from './services/create-article.service'
+import { EffectsModule } from '@ngrx/effects'
+import { CreateArticleEffects } from './store/createArticleEffects'
+import { StoreModule } from '@ngrx/store'
+import { reducers } from './store/createArticleReducer'
 
 const routes: Routes = [
   {
@@ -13,7 +17,13 @@ const routes: Routes = [
 ]
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ArticleFormModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ArticleFormModule,
+    EffectsModule.forFeature([CreateArticleEffects]),
+    StoreModule.forFeature('createArticle', reducers),
+  ],
   declarations: [CreateArticleComponent],
   providers: [CreateArticleService],
 })
---
+++ src/app/create-article/store/createArticleReducer.ts
@@ -0,0 +1,36 @@
+import { BackendErrors } from '../../shared/types/interfaces'
+import { Action, createReducer, on } from '@ngrx/store'
+import { createArticleErrorAction, createArticleRequestAction, createArticleSuccessAction } from './createArticleActions'
+import { ArticleState } from '../../article/store/articleReducer'
+
+export interface CreateArticleState {
+  isSubmitting: boolean
+  errors: BackendErrors | null
+}
+
+const initialState: CreateArticleState = {
+  isSubmitting: false,
+  errors: null,
+}
+
+const createArticleReducer = createReducer(
+  initialState,
+  on(createArticleRequestAction, (state, action) => ({
+    ...state,
+    isSubmitting: true,
+  })),
+  on(createArticleSuccessAction, (state, action) => ({
+    ...state,
+    isSubmitting: false,
+    errors: null,
+  })),
+  on(createArticleErrorAction, (state, action) => ({
+    ...state,
+    isSubmitting: false,
+    errors: action.errors,
+  }))
+)
+
+export function reducers(state: CreateArticleState, action: Action) {
+  return createArticleReducer(state, action)
+}
