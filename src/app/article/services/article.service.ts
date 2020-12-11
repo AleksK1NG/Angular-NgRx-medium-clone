9ca12f0885961df71a0e5608a3a0d1d717dc8af4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-11 13:44:42 +0300

    feat: add delete article action

--- src/app/article/services/article.service.ts
+++ src/app/article/services/article.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { environment } from '../../../environments/environment'
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +9,8 @@ import { HttpClient } from '@angular/common/http'
 export class ArticleService {
   constructor(private httpClient: HttpClient) {}
 
-
+  deleteArticle(slug: string): Observable<{}> {
+    const fullUrl = `${environment.apiUrl}/articles/${slug}`
+    return this.httpClient.delete<{}>(fullUrl)
+  }
 }
--- src/app/article/store/articleActions.ts
+++ src/app/article/store/articleActions.ts
@@ -5,3 +5,7 @@ import { Article } from '../../shared/types/interfaces'
 export const getArticleRequestAction = createAction(ArticleActionTypes.GET_ARTICLE_REQUEST, props<{ slug: string }>())
 export const getArticleSuccessAction = createAction(ArticleActionTypes.GET_ARTICLE_SUCCESS, props<{ article: Article }>())
 export const getArticleErrorAction = createAction(ArticleActionTypes.GET_ARTICLE_ERROR)
+
+export const deleteArticleRequestAction = createAction(ArticleActionTypes.DELETE_ARTICLE_REQUEST, props<{ slug: string }>())
+export const deleteArticleSuccessAction = createAction(ArticleActionTypes.DELETE_ARTICLE_SUCCESS)
+export const deleteArticleErrorAction = createAction(ArticleActionTypes.DELETE_ARTICLE_ERROR)
