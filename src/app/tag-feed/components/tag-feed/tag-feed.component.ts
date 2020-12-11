e5ec0ff0932e2e93eda3c64fbb2760784e279d3a
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-11 11:49:23 +0300

    feat: add TagFeedComponent tagName fix

--- src/app/tag-feed/components/tag-feed/tag-feed.component.ts
+++ src/app/tag-feed/components/tag-feed/tag-feed.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-tag-feed',
   templateUrl: './tag-feed.component.html',
   styleUrls: ['./tag-feed.component.scss'],
 })
-export class TagFeedComponent implements OnInit {
+export class TagFeedComponent implements OnInit, OnDestroy {
   apiUrl: string
   tagName: string
+  routeParams: Subscription
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe((params) => {
+    this.routeParams = this.route.params.subscribe((params) => {
+      this.tagName = params.slug
       this.apiUrl = `/articles?tag=${params.slug}`
-      console.log('CHANGE PARAMS ', params)
     })
+  }
+
+  ngOnDestroy(): void {
+    this.routeParams.unsubscribe()
   }
 }
