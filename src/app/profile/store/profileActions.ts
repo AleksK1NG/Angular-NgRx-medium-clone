63ccfc24e4245775a3440955aa0977db0aa7d9a3
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-12 10:46:37 +0300

    feat: add profile store actions and effects

---
+++ src/app/profile/store/profileActions.ts
@@ -0,0 +1,9 @@
+import { createAction, props } from '@ngrx/store'
+import { ProfileActionTypes } from './profileActionTypes'
+import { UserProfile } from '../../shared/types/interfaces'
+
+export const getProfileRequestAction = createAction(ProfileActionTypes.GET__PROFILE_REQUEST, props<{ slug: string }>())
+
+export const getProfileSuccessAction = createAction(ProfileActionTypes.GET__PROFILE_SUCCESS, props<{ userProfile: UserProfile }>())
+
+export const getProfileErrorAction = createAction(ProfileActionTypes.GET__PROFILE_ERROR)
---
+++ src/app/profile/store/profileEffects.ts
@@ -0,0 +1,28 @@
+import { Actions, createEffect, ofType } from '@ngrx/effects'
+import { ProfileService } from '../services/profile.service'
+import { getProfileErrorAction, getProfileRequestAction, getProfileSuccessAction } from './profileActions'
+import { Injectable } from '@angular/core'
+import { UserProfile } from '../../shared/types/interfaces'
+import { catchError, map, switchMap } from 'rxjs/operators'
+import { of } from 'rxjs'
+
+@Injectable()
+export class GetUserProfileEffect {
+  constructor(private actions$: Actions, private profileService: ProfileService) {}
+
+  getUserProfile$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(getProfileRequestAction),
+      switchMap(({ slug }) => {
+        return this.profileService.getUserProfile(slug).pipe(
+          map((userProfile: UserProfile) => {
+            return getProfileSuccessAction({ userProfile })
+          }),
+          catchError(() => {
+            return of(getProfileErrorAction())
+          })
+        )
+      })
+    )
+  )
+}
