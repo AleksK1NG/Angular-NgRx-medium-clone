b016b7ae15826f1c692190ddb9b040d136e5761d
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-12 10:51:03 +0300

    feat: add profile reducer

---
+++ src/app/profile/store/profileReducer.ts
@@ -0,0 +1,38 @@
+import { UserProfile } from '../../shared/types/interfaces'
+import { Action, createReducer, on } from '@ngrx/store'
+import { getProfileErrorAction, getProfileRequestAction, getProfileSuccessAction } from './profileActions'
+
+export interface ProfileState {
+  data: UserProfile | null
+  isLoading: boolean
+  error: string | null
+}
+
+const initialState: ProfileState = {
+  data: null,
+  isLoading: false,
+  error: null,
+}
+
+const profileReducer = createReducer(
+  initialState,
+  on(getProfileRequestAction, (state, action) => ({
+    ...state,
+    isLoading: true,
+  })),
+  on(getProfileSuccessAction, (state, action) => ({
+    ...state,
+    isLoading: false,
+    data: action.userProfile,
+    error: null,
+  })),
+  on(getProfileErrorAction, (state, action) => ({
+    ...state,
+    isLoading: false,
+    data: null,
+  }))
+)
+
+export function reducers(state: ProfileState, action: Action) {
+  return profileReducer(state, action)
+}
