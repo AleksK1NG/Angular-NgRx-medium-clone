8a010d4adb467b20a9196372a7a4aefe821440c4
Alexander Bryksin <alexander.bryksin@yandex.ru>
2020-12-12 10:46:37 +0300

    feat: add profile store actions and effects

---
+++ src/app/profile/store/profileActionTypes.ts
@@ -0,0 +1,5 @@
+export enum ProfileActionTypes {
+  GET__PROFILE_REQUEST = '[Profile] GET__PROFILE_REQUEST',
+  GET__PROFILE_SUCCESS = '[Profile] GET__PROFILE_SUCCESS',
+  GET__PROFILE_ERROR = '[Profile] GET__PROFILE_ERROR',
+}
